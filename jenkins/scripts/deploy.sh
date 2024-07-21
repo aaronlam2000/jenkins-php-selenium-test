734e2a03ce56f7a29aa3163b3adf5b78bdbd372d
Aaron Lam <aaronlammpn@gmail.com>
2024-07-21 14:00:34 +0800

    Updated path for deploy

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\ASUS\\Desktop\\Trimester 3\\ICT2216 Secure Software Development\\ICT2216 Labs\\Lab 7\\Lab 7b\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\ASUS\\Desktop\\SSD\Lab-7b\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
